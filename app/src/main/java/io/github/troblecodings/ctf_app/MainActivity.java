f03c112e8acef93373851064f62006068cf5c5d9
MrTroble <nifroh56@gmail.com>
2018-11-15 21:04:27 +0100

    Networking update

--- app/src/main/java/io/github/troblecodings/ctf_app/MainActivity.java
+++ app/src/main/java/io/github/troblecodings/ctf_app/MainActivity.java
@@ -2,12 +2,51 @@ package io.github.troblecodings.ctf_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.logging.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    public static Logger LOGGER;
+
+    private Networking networking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LOGGER = Logger.getLogger("io.github.troblecodings.ctf_app");
         setContentView(R.layout.activity_main);
+        this.networking = new Networking();
+        this.networking.execute("10.0.2.2");
+
+        // This is looking bad but there is probably no better way
+        findViewById(R.id.blue_team_player_1).setOnClickListener(this);
+        findViewById(R.id.blue_team_player_2).setOnClickListener(this);
+        findViewById(R.id.blue_team_player_3).setOnClickListener(this);
+        findViewById(R.id.blue_team_player_4).setOnClickListener(this);
+        findViewById(R.id.red_team_player_1).setOnClickListener(this);
+        findViewById(R.id.red_team_player_2).setOnClickListener(this);
+        findViewById(R.id.red_team_player_3).setOnClickListener(this);
+        findViewById(R.id.red_team_player_4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // This is looking bad but there is probably no better way
+        switch (v.getId()){
+            case R.id.blue_team_player_1: this.networking.sendData("blue:1"); break;
+            case R.id.blue_team_player_2: this.networking.sendData("blue:2"); break;
+            case R.id.blue_team_player_3: this.networking.sendData("blue:3"); break;
+            case R.id.blue_team_player_4: this.networking.sendData("blue:4"); break;
+            case R.id.red_team_player_1: this.networking.sendData("red:1"); break;
+            case R.id.red_team_player_2: this.networking.sendData("red:2"); break;
+            case R.id.red_team_player_3: this.networking.sendData("red:3"); break;
+            case R.id.red_team_player_4: this.networking.sendData("red:4"); break;
+        }
     }
 }
--- app/src/main/java/io/github/troblecodings/ctf_app/Networking.java
+++ app/src/main/java/io/github/troblecodings/ctf_app/Networking.java
@@ -1,4 +1,67 @@
 package io.github.troblecodings.ctf_app;
 
-public class Networking {
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+
+public class Networking extends AsyncTask<String, Void, Void> {
+
+    private SSLSocket socket;
+    private PrintWriter writer;
+    private Scanner scanner;
+    private ArrayList<String> msg_queue = new ArrayList<>();
+
+    @Override
+    protected Void doInBackground(String... local_ip) {
+        try {
+            startNetworking(local_ip[0]);
+        } catch (Exception e) {
+            MainActivity.LOGGER.log(Level.WARNING,"Networking error!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    private void startNetworking(String local_ip) throws Exception{
+        this.socket = (SSLSocket) SSLSocketFactory.getDefault().createSocket(local_ip, 555);
+        this.writer = new PrintWriter(this.socket.getOutputStream());
+        this.scanner = new Scanner(this.socket.getInputStream());
+        while (true){
+            if(msg_queue.size() > 0) {
+                for (String msg : msg_queue) {
+                    this.writer.println(msg);
+                    MainActivity.LOGGER.info("Message: " + msg);
+                    if (this.scanner.hasNextLine()) {
+                        String rsp = this.scanner.nextLine();
+                        MainActivity.LOGGER.info("Response: " + rsp);
+                    }
+                }
+            }
+        }
+    }
+
+    public void sendData(String str){
+        msg_queue.add(str);
+    }
+
+    public void close() {
+        try {
+            this.socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
